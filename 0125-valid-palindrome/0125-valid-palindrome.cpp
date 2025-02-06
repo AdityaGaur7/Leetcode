51040cf05a1cc89be925cdcd0c3484f0ec2213dc
Adgaur7 <121216575+AdityaGaur7@users.noreply.github.com>
2025-02-07 01:20:43 +0530

    Time: 3 ms (32.26%), Space: 9.9 MB (79.05%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-       int start=0;
+         int start=0;
        int end=s.size()-1;
        while(start<=end){
            if(!isalnum(s[start])){start++; continue;}
