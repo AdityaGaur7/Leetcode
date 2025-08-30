de8a74b48f6b54c32d5d181ccf9e405a47c58d61
Adgaur7 <121216575+AdityaGaur7@users.noreply.github.com>
2025-08-30 23:51:25 +0530

    Time: 3 ms (41.97%), Space: 20.4 MB (42.83%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,37 +1,35 @@
 class Solution {
-private:
-    void dfs(int i, vector<vector<int>>& adj, vector<int>& vis) {
-        vis[i] = 1;
-        for (auto it : adj[i]) {
-            if (!vis[it]) {
-                dfs(it, adj, vis);
-            }
-        }
-    }
-
 public:
-    int findCircleNum(vector<vector<int>>& mat) {
-        int n = mat.size(); // Use mat.size() instead of mat[0].size() to avoid potential issues
-        vector<vector<int>> adj(n); // Fix: Use vector<vector<int>> instead of VLAs
-        vector<int> vis(n, 0); // Fix: Use vector<int> for visited array
-
-        // Construct adjacency list
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (mat[i][j] == 1 && i != j) {
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
+void dfs(unordered_map<int,vector<int>>&mp,vector<bool>&vis,int i){
+   vis[i]=true;
+    
+    for(auto it:mp[i]){
+        if(vis[it]==false)
+         dfs(mp,vis,it);
+    }
+   
+    return ;
+}
+    int findCircleNum(vector<vector<int>>& g) {
+        int n = g.size();
+        int m = g[0].size();
+int cnt=0;
+unordered_map<int,vector<int>>mp;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(g[i][j]==1 && i!=j){
+                    mp[i].push_back(j);
                 }
             }
         }
-
-        int cnt = 0;
-        for (int i = 0; i < n; i++) {
-            if (!vis[i]) {
-                cnt++;
-                dfs(i, adj, vis);
-            }
+        vector<bool>vis(n,false);
+        for(int i=0;i<n;i++){
+                if(vis[i]==false){
+                    cnt++;
+                   dfs(mp,vis,i);
+                }   
         }
+        
         return cnt;
     }
 };
