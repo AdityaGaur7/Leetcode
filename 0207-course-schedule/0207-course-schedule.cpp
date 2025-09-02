1efa4abc08eea559f46af16314c2ff48e7b0c6c3
Adgaur7 <121216575+AdityaGaur7@users.noreply.github.com>
2025-09-03 00:36:59 +0530

    Time: 16 ms (7.38%), Space: 20.9 MB (11.94%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,39 +1,32 @@
-#include <bits/stdc++.h>
 class Solution {
 public:
+
+
     bool canFinish(int n, vector<vector<int>>& p) {
-    
-        vector<int> adj[2000];
-
-        for (auto it : p) {
-            adj[it[0]].push_back(it[1]);
+         unordered_map<int,vector<int>>mp;
+        vector<int>indegree(n,0);
+        for(auto e:p){
+            mp[e[0]].push_back(e[1]);
+            indegree[e[1]]++;
         }
-        int indegree[2000] = {0};
-        for (int i = 0; i < n; i++) {
-            for (auto it : adj[i]) {
-                indegree[it]++;
-            }
+      int cnt=0;
+        queue<int>q;
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0)q.push(i);
         }
-        queue<int> q;
-        for (int i = 0; i < n; i++) {
-            if (indegree[i] == 0)
-                q.push(i);
-        }
-        vector<int> topo;
-        while (!q.empty()) {
-            int node = q.front();
+        
+        while(!q.empty()){
+            int x= q.front();
             q.pop();
-            topo.push_back(node);
-
-            for (auto it : adj[node]) {
+            cnt++;
+            for(auto it:mp[x]){
                 indegree[it]--;
-                if (indegree[it] == 0)
-                    q.push(it);
+                if(indegree[it]==0)q.push(it);
             }
         }
-        if (topo.size() == n)
-            return true;
-            else
-        return false;
+        // cout<<ans.size();
+        if(cnt==n)return 1;
+        else return 0;
+
     }
 };
